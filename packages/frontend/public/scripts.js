b01cde823d8dd90c3ad0e828c55901abd2fc1267
Miguel Morales <mimoralesga@gmail.com>
2024-05-27 17:30:16 -0500

    icons added

--- packages/frontend/public/scripts.js
+++ packages/frontend/public/scripts.js
@@ -76,8 +76,8 @@ function render() {
   if (selected) {
     $header.innerHTML = "";
     const $complete = document.createElement("button");
-    $complete.innerText = "Completar";
-    $complete.className = "button button-small inline-block ml-2";
+    $complete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg> Completar`;
+    $complete.className = "button button-small inline-flex ml-2";
     $header.appendChild(
       document.createTextNode(
         `${selected} elemento${selected > 1 ? "s" : ""} seleccionado${selected > 1 ? "s" : ""}`,
